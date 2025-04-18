39d8ca4096e9e68958990a9e1105633a2344fcee
internship07 <humerafirdousmakandar@gmail.com>
2025-04-18 21:51:03 +0530

    Add files via upload

---
+++ numbertask4.php
@@ -0,0 +1,10 @@
+<?php 
+$num1 = "443.3432";
+$num2 = "7543.829";
+echo 'Initializing ceiling value: ' . ceil($num1) . "<br>";
+echo 'Initializing floor value: ' . floor($num2) . "<br>";
+
+// Generate a random number between 500 and 5000
+$randomNumber = rand(500, 5000);
+echo 'Random number between 500 and 5000: ' . $randomNumber . "<br>";
+?>
