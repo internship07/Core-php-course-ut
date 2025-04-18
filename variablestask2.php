1706d089fdfbcc7c534b6bef3a2694be1d0cf5b9
internship07 <humerafirdousmakandar@gmail.com>
2025-04-18 20:25:23 +0530

    Add files via upload

---
+++ variablestask2.php
@@ -0,0 +1,17 @@
+<?php 
+$collagename = "Nehru BBA and BCA College Hubli";
+$regno = "U02ATS0065";
+$branch = "BCA";
+
+// Correct usage of define() function
+define('MOBILE', '1234567890');
+define('DEPARTMENT', 'BCA');
+
+// Corrected echo statements
+echo "$collagename<br>";
+echo "$regno<br>";
+echo "$branch<br>";
+echo MOBILE . "<br>";
+echo DEPARTMENT . "<br>";
+echo "Address - Mantur Road, Hubballi";
+?>
